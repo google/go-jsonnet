6a29151819816557238bd56f984ee2401c3c59bf
Joe Beda <joe.github@bedafamily.com>
2016-02-20 18:31:21 -0800

    Add copyright notice to all files.
    With this commit, I grant a Copyright License in this project submitted up until now to Google under the terms of the Google CLA.  See https://cla.developers.google.com/about/google-individual.

--- parser.go
+++ parser.go
@@ -1,3 +1,18 @@
+/*
+Copyright 2016 Google Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package jsonnet
 
 import (
@@ -90,7 +105,7 @@ func (p *parser) parseCommaList(end tokenKind, elementKind string) (*token, astN
 	got_comma := false
 	first := true
 	for {
-		next := p.peek();
+		next := p.peek()
 		if !first && !got_comma {
 			if next.kind == tokenComma {
 				p.pop()
@@ -205,10 +220,10 @@ func (p *parser) parse(prec precedence) (astNode, error) {
 				return nil, err
 			}
 			return &astFunction{
-				astNodeBase: astNodeBase{locFromTokenAST(begin, body)},
-				parameters:  params,
+				astNodeBase:   astNodeBase{locFromTokenAST(begin, body)},
+				parameters:    params,
 				trailingComma: got_comma,
-				body: body,
+				body:          body,
 			}, nil
 		} else {
 			return nil, makeStaticError(fmt.Sprintf("Expected ( but got %v", next), next.loc)
