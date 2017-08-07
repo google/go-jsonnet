370534ccc4a44ade92949b5e147ed57205cf1dd5
Stanisław Barzowski <sbarzowski@google.com>
2017-08-07 14:06:46 -0400

    Basic commandline (so we can start testing compatibility)

---
+++ tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source tests_path.source
+export DISABLE_EXT_PARAMS=true
+export DISABLE_LIB_TESTS=true
+export DISABLE_FMT_TESTS=true
+export JSONNET_BIN="$PWD/jsonnet/jsonnet"
+cd "$TESTS_PATH"
+./tests.sh
