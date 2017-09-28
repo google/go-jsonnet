b4b5ef769e7bbbf352e8379eb2dc3cb259978332
Stanisław Barzowski <sbarzowski@google.com>
2017-09-28 12:25:12 -0400

    Cleaner parseComprehensionSpecs return values

--- parser/parser.go
+++ parser/parser.go
@@ -353,7 +353,7 @@ func (p *parser) parseObjectRemainder(tok *token) (ast.Node, *token, error) {
 			if field.Kind != ast.ObjectFieldExpr {
 				return nil, nil, MakeStaticError("Object comprehensions can only have [e] fields.", next.loc)
 			}
-			spec, _, last, err := p.parseComprehensionSpecs(tokenBraceR)
+			spec, last, err := p.parseComprehensionSpecs(tokenBraceR)
 			if err != nil {
 				return nil, nil, err
 			}
@@ -537,53 +537,58 @@ func (p *parser) parseObjectRemainder(tok *token) (ast.Node, *token, error) {
 }
 
 /* parses for x in expr for y in expr if expr for z in expr ... */
-func (p *parser) parseComprehensionSpecs(end tokenKind) (*ast.ForSpec, *ast.ForSpec, *token, error) {
-	var ifSpecs []ast.IfSpec
+func (p *parser) parseComprehensionSpecs(end tokenKind) (*ast.ForSpec, *token, error) {
+	var parseComprehensionSpecsHelper func() (*ast.ForSpec, *ast.ForSpec, *token, error)
+	parseComprehensionSpecsHelper = func() (*ast.ForSpec, *ast.ForSpec, *token, error) {
+		var ifSpecs []ast.IfSpec
 
-	varID, err := p.popExpect(tokenIdentifier)
-	if err != nil {
-		return nil, nil, nil, err
-	}
-	id := ast.Identifier(varID.data)
-	_, err = p.popExpect(tokenIn)
-	if err != nil {
-		return nil, nil, nil, err
-	}
-	arr, err := p.parse(maxPrecedence)
-	if err != nil {
-		return nil, nil, nil, err
-	}
-	forSpec := &ast.ForSpec{
-		VarName: id,
-		Expr:    arr,
-	}
-
-	maybeIf := p.pop()
-	for ; maybeIf.kind == tokenIf; maybeIf = p.pop() {
-		cond, err := p.parse(maxPrecedence)
+		varID, err := p.popExpect(tokenIdentifier)
 		if err != nil {
 			return nil, nil, nil, err
 		}
-		ifSpecs = append(ifSpecs, ast.IfSpec{
-			Expr: cond,
-		})
-	}
-	forSpec.Conditions = ifSpecs
-	if maybeIf.kind == end {
-		return forSpec, forSpec, maybeIf, nil
-	}
+		id := ast.Identifier(varID.data)
+		_, err = p.popExpect(tokenIn)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		arr, err := p.parse(maxPrecedence)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		forSpec := &ast.ForSpec{
+			VarName: id,
+			Expr:    arr,
+		}
 
-	if maybeIf.kind != tokenFor {
-		return nil, nil, nil, MakeStaticError(
-			fmt.Sprintf("Expected for, if or %v after for clause, got: %v", end, maybeIf), maybeIf.loc)
-	}
+		maybeIf := p.pop()
+		for ; maybeIf.kind == tokenIf; maybeIf = p.pop() {
+			cond, err := p.parse(maxPrecedence)
+			if err != nil {
+				return nil, nil, nil, err
+			}
+			ifSpecs = append(ifSpecs, ast.IfSpec{
+				Expr: cond,
+			})
+		}
+		forSpec.Conditions = ifSpecs
+		if maybeIf.kind == end {
+			return forSpec, forSpec, maybeIf, nil
+		}
 
-	lastSpec, nextSpec, lastToken, err := p.parseComprehensionSpecs(end)
-	if err != nil {
-		return nil, nil, nil, err
+		if maybeIf.kind != tokenFor {
+			return nil, nil, nil, MakeStaticError(
+				fmt.Sprintf("Expected for, if or %v after for clause, got: %v", end, maybeIf), maybeIf.loc)
+		}
+
+		lastSpec, nextSpec, lastToken, err := parseComprehensionSpecsHelper()
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		nextSpec.Outer = forSpec
+		return lastSpec, forSpec, lastToken, nil
 	}
-	nextSpec.Outer = forSpec
-	return lastSpec, forSpec, lastToken, nil
+	outer, _, lastToken, err := parseComprehensionSpecsHelper()
+	return outer, lastToken, err
 }
 
 // Assumes that the leading '[' has already been consumed and passed as tok.
@@ -612,7 +617,7 @@ func (p *parser) parseArray(tok *token) (ast.Node, error) {
 	if next.kind == tokenFor {
 		// It's a comprehension
 		p.pop()
-		spec, _, last, err := p.parseComprehensionSpecs(tokenBracketR)
+		spec, last, err := p.parseComprehensionSpecs(tokenBracketR)
 		if err != nil {
 			return nil, err
 		}
