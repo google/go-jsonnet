f370ba1b451abf6e668311ba40456050220518d1
Marcelo Cantos <marcelo.cantos@gmail.com>
2018-02-25 21:49:54 +1100

    Remove redundant package comment

--- vm.go
+++ vm.go
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package jsonnet implements the Jsonnet language.
 package jsonnet
 
 import (
