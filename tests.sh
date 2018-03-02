e5062c82428ab4818f8b28c2723c8569c5ecddc2
Marcelo Cantos <marcelo.cantos@gmail.com>
2018-03-02 19:11:32 +1100

    Add C++-Jsonnet submodule for tests

--- tests.sh
+++ tests.sh
@@ -5,10 +5,12 @@ set -e
 export IMPLEMENTATION=go
 
 (cd jsonnet; go build)
-source tests_path.source
+
 export DISABLE_LIB_TESTS=true
 export DISABLE_FMT_TESTS=true
 export DISABLE_ERROR_TESTS=true
 export JSONNET_BIN="$PWD/jsonnet/jsonnet"
-cd "$TESTS_PATH"
-./tests.sh
+
+git submodule update --recursive cpp-jsonnet
+cd cpp-jsonnet
+exec ./tests.sh
