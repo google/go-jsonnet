2205595d7f60304826c57bcdc9476c85b5f1adf9
Dave Cunningham <sparkprime@gmail.com>
2018-07-19 15:13:41 -0400

    Update cpp-jsonnet & bump version

--- vm.go
+++ vm.go
@@ -191,5 +191,5 @@ func SnippetToAST(filename string, snippet string) (ast.Node, error) {
 
 // Version returns the Jsonnet version number.
 func Version() string {
-	return "v0.10.0"
+	return "v0.11.2"
 }
