915c00bfdad9ba9af2a335452af6d2cee57cb7f8
Stanisław Barzowski <stanislaw.barzowski@gmail.com>
2020-02-10 00:14:45 +0100

    Remove old release mechanism from Travis

--- travisBuild.sh
+++ travisBuild.sh
@@ -5,21 +5,5 @@ run_tests() {
   ./tests.sh --skip-go-test
 }
 
-release() {
-  env VERSION=$TRAVIS_TAG ./release.sh
-}
-
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  # Pull Requests.
-  echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  run_tests
-elif [ "$TRAVIS_TAG" == "" ]; then
-  # Pushed branches.
-  echo -e "Build Branch $TRAVIS_BRANCH"
-  run_tests
-else
-  # $TRAVIS_PULL_REQUEST == "false" and $TRAVIS_TAG != "" -> Releases.
-  echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  release
-fi
+run_tests
 
