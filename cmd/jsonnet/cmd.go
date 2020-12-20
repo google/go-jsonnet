7a5954df41516200f9e781928dcad0b102fef166
Stanisław Barzowski <stanislaw.barzowski@gmail.com>
2020-12-20 21:31:31 +0100

    Prevent keeping files open for too long.
    Fixes #485.

--- cmd/jsonnet/cmd.go
+++ cmd/jsonnet/cmd.go
@@ -345,17 +345,7 @@ func writeMultiOutputFiles(output map[string]string, outputDir, outputFile strin
 			}
 		}
 
-		f, err := os.Create(filename)
-		if err != nil {
-			return err
-		}
-		defer func() {
-			if ferr := f.Close(); ferr != nil {
-				err = ferr
-			}
-		}()
-
-		_, err = f.WriteString(newContent)
+		err = ioutil.WriteFile(filename, []byte(newContent), 0666)
 		if err != nil {
 			return err
 		}
