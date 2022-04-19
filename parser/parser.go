56deb20a9292a04874d133af728143a9db4e6cb6
Michal Jurečko <michal.jurecko@gmail.com>
2022-04-19 15:51:59 +0200

    Parser: add fileName argument to SnippetToAst/SnippetToRawAST methods

--- parser/parser.go
+++ parser/parser.go
@@ -6,12 +6,12 @@ import (
 	"github.com/google/go-jsonnet/internal/program"
 )
 
-func SnippetToRawAST(snippet string) (ast.Node, ast.Fodder, error) {
-	return parser.SnippetToRawAST("", "", snippet)
+func SnippetToRawAST(snippet, fileName string) (ast.Node, ast.Fodder, error) {
+	return parser.SnippetToRawAST(ast.DiagnosticFileName(fileName), fileName, snippet)
 }
 
-func SnippetToAst(snippet string) (ast.Node, error) {
-	return program.SnippetToAST("", "", snippet)
+func SnippetToAst(snippet, fileName string) (ast.Node, error) {
+	return program.SnippetToAST(ast.DiagnosticFileName(fileName), fileName, snippet)
 }
 
 func PreprocessAst(node *ast.Node) error {
