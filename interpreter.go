ef930fd8a51c5e86201137b0003500eb1a69e84b
Brian Kamotho <lordkamotho@gmail.com>
2024-01-11 19:41:42 +0300

    Fix interface conversion from int types to float64

--- interpreter.go
+++ interpreter.go
@@ -950,7 +950,8 @@ func jsonToValue(i *interpreter, v interface{}) (value, error) {
 	case bool:
 		return makeValueBoolean(v), nil
 	case int, int8, int16, int32, int64:
-		return makeDoubleCheck(i, v.(float64))
+		val, _ := v.(int64)
+		return makeDoubleCheck(i, float64(val))
 	case float64:
 		return makeDoubleCheck(i, v)
 
