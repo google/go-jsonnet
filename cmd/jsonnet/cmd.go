c23a8b76e7b2275afd15e54cbe19616323a0998e
b13rg <13488329+B13rg@users.noreply.github.com>
2025-04-05 21:50:54 -0700

    set profilerOpts var in main

--- cmd/jsonnet/cmd.go
+++ cmd/jsonnet/cmd.go
@@ -395,8 +395,8 @@ func main() {
 	cmd.StartCPUProfile()
 	defer cmd.StopCPUProfile()
 
-	jsonnet.StartStackProfile()
-	defer jsonnet.StopStackProfile()
+	profilerOpts := jsonnet.StartStackProfile()
+	defer jsonnet.StopStackProfile(profilerOpts)
 
 	vm := jsonnet.MakeVM()
 	vm.ErrorFormatter.SetColorFormatter(color.New(color.FgRed).Fprintf)
